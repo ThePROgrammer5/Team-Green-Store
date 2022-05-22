c7649a63e1ff26363ca908348f86da69563c23f4
The PROgrammer / <codingplusgaming@gmail.com>
2022-05-22 13:16:42 -0700

    done

--- store.js
+++ store.js
@@ -21,10 +21,11 @@ function recipt(rec) {
 
 function addToKart(item) {
     alert(item);
-    alert(JSON.stringify(item))
+    alert(JSON.stringify(item));
     var number = prompt("How many would you like to order?");
-    kart.item = kart.item + number;
-    var cost = stock.item.price; // calculates the cost of item in dollars, cost in cents / 100 = cost in dollars
+    kart.item_name_no_spaces = kart.item_name_no_spaces + number;
+    stock.item_name_no_spaces.amount = stock.item_name_no_spaces.amount -1;
+    var cost = stock.item_name_no_spaces.price; // calculates the cost of item in dollars, cost in cents / 100 = cost in dollars
     total = total + cost; // add the items cost to the total
 }
 
