66b4e28a99ad8bdc62fa0c3b1dbeca8ee843a6fa
The PROgrammer / <codingplusgaming@gmail.com>
2022-05-22 12:33:10 -0700

    done

---
+++ essential.js
@@ -0,0 +1,10 @@
+var stock = {
+    item_name_no_spaces: {
+        amount: amount_in_stock_must_be_number,
+        price: price_of_item_must_be_in_dollar_format_include_tax,
+    },
+    item_name_no_spaces_cant_be_same_as_other_item: {
+        amount: amount_in_stock_must_be_number,
+        price: price_of_item_must_be_in_dollar_format_include_tax,
+    },
+}
--- store.js
+++ store.js
@@ -7,7 +7,14 @@ var kart = {
 };
 
 var stock = {
-    
+    item_name_no_spaces: {
+        amount: amount_in_stock_must_be_number,
+        price: price_of_item_must_be_in_dollar_format_include_tax,
+    },
+    item_name_no_spaces: {
+        amount: amount_in_stock_must_be_number,
+        price: price_of_item_must_be_in_dollar_format_include_tax,
+    },
 }
 
 function orderN() {
